fac36700aafbdf0ff75bffc531267c61ea4e0828
Ayesha Khan <akhan1231@hotmail.com>
2019-12-01 13:25:14 -0800

    Button program state
    @State allows that value to be stored separately by SwiftUI in a place that can be modified.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,15 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     Text("Hello, World!")
                 }
+                Button("Tap Count: \(tapCount)") {
+                    self.tapCount += 1
+                }
             }
             .navigationBarTitle("SwiftUI", displayMode: .inline)
         }
+        
+        
     }
 }
 
